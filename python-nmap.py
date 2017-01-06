6e4dcaba3453291ea0289b2902214745763136c6
nonnimartin <nonnimartin@gmail.com>
2017-01-05 23:19:55 -0800

    Added db minimal create statment and basic sql

--- python-nmap.py
+++ python-nmap.py
@@ -2,16 +2,38 @@ import nmap
 import json
 import traceback
 import sys
+import sqlite3
 
-ip = '10.100.2.203'
+#Set IP address
+ip   = '192.168.1.72'
+args = '-sS -O' 
+#Create database connection object
+db   = './data.db'
+conn = sqlite3.connect(db)
+c = conn.cursor()
 
 try:
     nm = nmap.PortScanner()
-    scan = nm.scan(hosts=ip, arguments='-sS -O')
-    scan = json.dumps(scan)
-    print ("Hostname = " + nm[ip].hostname())
-    print ("Scan information : " + str(scan))
-#except Exception as e:
-#    print (e)
+    scan = nm.scan(hosts=ip, arguments=args)
+    scan_json = json.dumps(scan)
+    #print ("Hostname = " + nm[ip].hostname())
+    #print ("Scan information : " + str(scan))
 except Exception as e:
     print (traceback.format_exception(None, e, e.__traceback__),  file=sys.stderr, flush=True)
+
+os          = "fixThisSoItCanBeEmpty"
+device_type = 'test'
+mac         = scan['scan'][ip]['addresses']['mac']
+scan_time   = scan['nmap']['scanstats']['timestr']
+vendor      = scan['scan'][ip]['vendor'][mac]
+command     = scan['nmap']['command_line']
+
+
+try:
+        c.execute('INSERT INTO scan_data(ip, os, device_type, mac, scan_time, vendor, command) VALUES (?, ?, ?, ?, ?, ?, ?)', [ip, os, device_type, mac, scan_time, vendor, command])
+except sqlite3.IntegrityError:
+        print('Error inserting new data')
+
+# Committing changes/close
+conn.commit()
+conn.close()
